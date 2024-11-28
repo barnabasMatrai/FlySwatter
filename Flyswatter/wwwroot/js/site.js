db5fc2f53615a77ff141ff502335617f1e4f8bba
Takacs, Zsolt <zsolt.takacs.ext@siemens-energy.com>
2024-11-28 08:05:11 +0100

    Prevent pinch zoom and context menu

--- Flyswatter/wwwroot/js/site.js
+++ Flyswatter/wwwroot/js/site.js
@@ -1,4 +1,26 @@
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
-// Write your JavaScript code.
+// Prevent opening right-click menu
+document.addEventListener('contextmenu', event => event.preventDefault());
+
+// Prevent accidental zoom
+document.addEventListener(
+    "wheel",
+    function touchHandler(e) {
+        if (e.ctrlKey) {
+            e.preventDefault();
+        }
+    }, { passive: false }
+);
+
+// Prevent multitouch events (accidental pinch-zoom)
+window.addEventListener("touchstart", touchHandler, false); 
+
+function touchHandler(event) {
+    if (event.touches.length > 1) {
+        //the event is multi-touch
+        //you can then prevent the behavior
+        event.preventDefault()
+    }
+}
